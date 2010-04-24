92d9c28900ce0d66f1d947f79ea240e860685016
banane <banane@94d317c6-c18f-8aa6-569c-5969cc96d0ad>
2010-04-24 21:19:38 +0000

    initial load

---
+++ trunk/Fortune.m
@@ -0,0 +1,17 @@
+// 
+//  Fortune.m
+//  Cootie
+//
+//  Created by Anna on 4/12/10.
+//  Copyright 2010 Apple Inc. All rights reserved.
+//
+
+#import "Fortune.h"
+
+
+@implementation Fortune 
+
+@dynamic FortunePosition;
+@dynamic FortuneString;
+
+@end
